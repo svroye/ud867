b647475bfa507ad5c7ebacddaa214f4dda6abab3
Stevenvr <vanroyesteven@gmail.com>
2018-05-21 18:29:48 +0200

    Re-download and solve 3.08

---
+++ 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/paid/java/com/udacity/gradle/flavorspecificactivity/paid/JokeActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.flavorspecificactivity.paid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.gradle.flavorspecificactivity.R;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+}
